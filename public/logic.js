008ca7968b8aa52b5f03a5c0ffebe6e2991dba64
Gabe Rodriguez <grodriguez@grodriguez-macbookpro3.roam.corp.google.com>
2016-11-07 15:05:32 +0100

    moar

--- public/logic.js
+++ public/logic.js
@@ -1,6 +1,16 @@
 'use strict';
 
+class Grid {
+	isAlive(x, y) {
+		return false;
+	}
+}
+
+function converter (x,y) {
+  return String(x) + '_' + String(y);
+}
 
 module.exports = {
-
+	Grid: Grid,
+  converter: converter
 };
--- spec/jasmineTests/spec.js
+++ spec/jasmineTests/spec.js
@@ -4,7 +4,14 @@ const conway =  require('../../public/logic.js');
 
 describe("Part 1", function() {
   it("successfulGridCreation", function() {
-    expect(conway.createGrid(5, 5)).toEqual(true);
+    expect(new conway.Grid().isAlive(0, 0)).toBe(false);
+  });
+
+});
+
+describe("converter", function() {
+  it("converts correctly", function() {
+    expect(conway.converter(2,3)).toEqual('2_3');
   });
 
 });
